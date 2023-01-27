19f9395c43d2174632a0d66fecd1387e7deaac52
Sheryl H <sheryl_hu0806@hotmail.com>
2023-01-27 20:26:53 +0000

    Created method to create new account

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,30 @@
+package DAO; 
+
+import Model.Account; 
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDAO {
+    public Account createAccount(Account account) {
+        Connection connection = ConnectionUtil.getConnection(); 
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)"; 
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS); 
+
+            preparedStatement.setString(1, account.getUsername()); 
+            preparedStatement.setString(2, account.getPassword()); 
+            preparedStatement.executeUpdate(); 
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys(); 
+            if(pkeyResultSet.next()){
+                int generated_account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage()); 
+        }
+        return null; 
+    }
+}
