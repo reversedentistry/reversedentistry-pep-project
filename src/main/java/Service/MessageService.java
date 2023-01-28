469eadcdad16599438cfd425df8da71edf5687d9
Sheryl H <sheryl_hu0806@hotmail.com>
2023-01-28 00:52:16 +0000

    Added in more conditions when adding new message

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -21,7 +21,7 @@ public class MessageService {
 
     // Check Message object for valid account ID before fully creating 
     public Message createMessage(Message message) {
-        if (accountDAO.getAccount(message.getPosted_by()) != null) {
+        if (accountDAO.getAccount(message.getPosted_by()) != null && message.getMessage_text().length() != 0) {
             return messageDAO.createMessage(message); 
         } else {
             return null; 
