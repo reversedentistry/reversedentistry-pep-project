8b76c61afb370dce20a4793077f1312205ac11d3
Sheryl H <sheryl_hu0806@hotmail.com>
2023-01-28 00:23:49 +0000

    Created method to make new account

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,24 @@
+package Service;
+
+import DAO.AccountDAO;
+import Model.Account;
+
+public class AccountService {
+    public AccountDAO accountDAO; 
+
+    public AccountService() {
+        accountDAO = new AccountDAO(); 
+    }
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO; 
+    }
+
+    public Account createAccount(Account account) {
+        if (account.getUsername() != null && account.getPassword().length() >= 4 && accountDAO.getUsername(account.getUsername()) == null) {
+            return accountDAO.createAccount(account); 
+        } else {
+            return null; 
+        }
+    }
+}
